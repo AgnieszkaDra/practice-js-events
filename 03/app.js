fe06dd0b03a54e62ca9bd87aa12058b6cd702f6d
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-11-08 14:31:43 +0100

    solution after show result

--- 03/app.js
+++ 03/app.js
@@ -1,29 +1,50 @@
 
-const buttons = document.querySelectorAll('button')
-console.log(buttons)
+// const buttons = document.querySelectorAll('button')
+// console.log(buttons)
 
-let myArray = Array.from(buttons)
+// let myArray = Array.from(buttons)
 
-console.log(myArray)
+// console.log(myArray)
 
-
-
-function showText(el) {
-    for (var i=0, l=el.length; i<l; i++) {
+// function showText(el) {
+//     for (var i=0, l=el.length; i<l; i++) {
         
-        function Click(e){
-            e.currentTarget.innerText = 'clicked'
-            console.log('clicked')
-            e.currentTarget.removeEventListener('click', Click)
-        }
+//         function Click(e){
+//             e.currentTarget.innerText = 'clicked' 
+//             console.log('clicked',this)
+//             e.currentTarget.removeEventListener('click', Click)
+//         }
 
-        el[i].addEventListener('click', Click)
+//         el[i].addEventListener('click', Click)
         
-    } 
+//     } 
+// }
+
+// showText(myArray) 
+
+// after check solution 
+
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+
+    console.log('DOM')
+
+    const btnsList = document.querySelectorAll('button')
+    btnsList.forEach(function(btn){
+        //console.log('clicked', this, e.currentTarget)
+        btn.addEventListener('click', handleClick)
+    })
 }
 
-showText(myArray) 
-
+const TEXT = 'clicked'
+function handleClick(e){
+    //const element = this
+    const element = e.currentTarget
+    console.log(TEXT, this, element)
+    element.innerText = 'clicked'
+    element.removeEventListener('click', handleClick)
+}
 
 
 
